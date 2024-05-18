f51472898f6bc241f935d4084f28cec599e8aecb
NZOUKEU LETA Ralph Yvan <ralph.nzoukeu@groupe-esigelec.org>
2024-05-18 17:10:28 +0200

    mise à jour

--- ProjetRFID/ProjetRFID/Controllers/HomeController.cs
+++ ProjetRFID/ProjetRFID/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ using NuGet.Protocol;
 using System.Security.Claims;
 using System.Drawing;
 using Microsoft.DotNet.Scaffolding.Shared.CodeModifier.CodeChange;
+using System.Text.Json;
 
 
 namespace ProjetRFID.Controllers
@@ -54,8 +55,8 @@ namespace ProjetRFID.Controllers
             return View();
         }
 
-        [HttpPost]
-        /** public IActionResult Traitement([FromBody] JsonElement formDataArray)
+
+        public IActionResult Traitement([FromBody] JsonElement formDataArray)
          {
              // Vérifiez si le JSON est un objet
              if (formDataArray.ValueKind == JsonValueKind.Object)
@@ -128,121 +129,129 @@ namespace ProjetRFID.Controllers
 
              // Vous pouvez également renvoyer les données pour confirmer qu'elles ont été reçues correctement
              return Ok(formDataArray);
-         }**/
-        
-        public async Task<ActionResult> Traitement(string checkbox_Analytique, string checkbox_KNN, string checkbox_RandomForest, string checkbox_SVM, int n_neighbors, string weight, string metric,float p,
-            string metric_params, string algorithm,int leaf_size,int n_estimators,string criterion,
-            int min_samples_split,int min_samples_leaf,float min_weight_fraction_leaf,int max_leaf_nodes,
-            float min_impurity_decrease,int n_jobs,int entier_detail,int max_depth,float C,string kernel,string gamma,
-            float coef0, float tol, float cache_size,int max_iter, string decision_function_shape)
-        {
-            if (checkbox_Analytique == "Analytique")
-            {
-                Analytique analytique = new Analytique();
-                _context.Analytique.Add(analytique);
-                await _context.SaveChangesAsync();
-                Simulation simulation = new Simulation
-                {
-                    UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
-                    idA = analytique.id,
-                    time = DateTime.Now,
-                };
+         }
+        [HttpPost]
+         public async Task<ActionResult> Traitement(string checkbox_Analytique, string checkbox_KNN, string checkbox_RandomForest, string checkbox_SVM, int n_neighbors, string weight, string metric,float p,
+             string metric_params, string algorithm,int leaf_size,int n_estimators,string criterion,
+             int min_samples_split,int min_samples_leaf,float min_weight_fraction_leaf,int max_leaf_nodes,
+             float min_impurity_decrease,int n_jobs,int entier_detail,int max_depth,float C,string kernel,string gamma,
+             float coef0, float tol, float cache_size,int max_iter, string decision_function_shape)
+         {
+             Console.WriteLine(gamma);
+             if (checkbox_Analytique == "Analytique")
+             {
+                 Analytique analytique = new Analytique()
+                 {
+                 };
+                 _context.Analytique.Add(analytique);
+                 await _context.SaveChangesAsync();
+                 
+                 Simulation simulation = new Simulation
+                 {
+                     UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
+                     idA = analytique.id,
+                     time = DateTime.Now,
+                 };
 
-                _context.Simulation.Add(simulation);
+                _context.Add(simulation);
 
 
-                await _context.SaveChangesAsync();
+                 await _context.SaveChangesAsync();
 
-                await _context.SaveChangesAsync();
-            }
-           
-            if (checkbox_KNN == "KNN")
-            {
-                KNN knn = new KNN();
-                {
-                    n_neighbors = knn.n_neighbors;
-                    weight = knn.weight.ToString();
-                    metric = knn.metric.ToString();
-                    p = knn.p;
-                    metric_params=knn.metric_params.ToString();
-                    algorithm=knn.algorithm.ToString();
-                    leaf_size=knn.leaf_size;
-                    _context.KNN.Add(knn);
-                    await _context.SaveChangesAsync();
-                    Simulation simulation = new Simulation
-                    {
-                        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
-                        idk = knn.id,
-                        time = DateTime.Now,
-                    };
-
-                    _context.Simulation.Add(simulation);
+                
+             }
+            
+             if (checkbox_KNN == "KNN")
+             {
+                 KNN knn = new KNN();
+                 {
+                      knn.n_neighbors= n_neighbors;
+                     knn.weight= weight;
+                     knn.metric = metric;
+                     knn.p = p;
+                     knn.metric_params = metric_params;
+                     knn.algorithm = algorithm;
+                     knn.leaf_size = leaf_size;
+                 };
+                     _context.KNN.Add(knn);
                      await _context.SaveChangesAsync();
-                };
+                     Simulation simulation = new Simulation
+                     {
+                         UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
+                         idk = knn.id,
+                         time = DateTime.Now,
+                         
+                     };
 
-            }
-            if (checkbox_RandomForest == "RandomForest")
-            {
-                Random_Forest randomforest = new Random_Forest();
-                {
-                    n_estimators=randomforest.n_estimators;
-                    criterion = randomforest.criterion.ToString();
-                    min_samples_split = randomforest.min_samples_split;
-                    min_samples_leaf = randomforest.min_samples_leaf;
-                    min_weight_fraction_leaf = randomforest.min_weight_fraction_leaf;
-                    max_leaf_nodes = randomforest.max_leaf_nodes;
-                    min_impurity_decrease = randomforest.min_impurity_decrease;
-                    n_jobs = randomforest.n_jobs;
-                    entier_detail = randomforest.entier_detail;
-                    max_depth=randomforest.max_depth;
-                    _context.Random_Forest.Add(randomforest);
-                    await _context.SaveChangesAsync();
-                    Simulation simulation = new Simulation
-                    {
-                        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
-                        idR = randomforest.id,
-                        time = DateTime.Now,
-                    };
+                     _context.Add(simulation);
+                      await _context.SaveChangesAsync();
+                 
 
-                    _context.Simulation.Add(simulation);
-                    await _context.SaveChangesAsync();
-                };
+             }
+             if (checkbox_RandomForest == "RandomForest")
+             {
+                 Random_Forest randomforest = new Random_Forest();
+                 {
+                     randomforest.n_estimators= n_estimators;
+                     randomforest.criterion = criterion;
+                     randomforest.min_samples_split = min_samples_split;
+                     randomforest.min_samples_leaf = min_samples_leaf;
+                     min_weight_fraction_leaf = randomforest.min_weight_fraction_leaf;
+                     randomforest.max_leaf_nodes = max_leaf_nodes;
+                     randomforest.min_impurity_decrease = min_impurity_decrease;
+                     randomforest.n_jobs = n_jobs;
+                     randomforest.entier_detail = entier_detail;
+                     randomforest.max_depth = max_depth;
+                 };
+                 _context.Random_Forest.Add(randomforest);
+                     await _context.SaveChangesAsync();
+                     Simulation simulation = new Simulation
+                     {
+                         UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
+                         idR = randomforest.id,
+                         time = DateTime.Now,
+                     };
 
-
-            }
-             if (checkbox_SVM == "SVM")
-            {
-                SVM svm = new SVM();
-                {
-
-                    C = svm.C;
-                    kernel=svm.kernel.ToString();
-                    gamma=svm.gamma.ToString();
-                    coef0 = svm.coef0;
-                    tol=svm.tol;
-                    cache_size=svm.cache_size;
-                    max_iter=svm.max_iter;
-                    decision_function_shape=svm.decision_function_shape;
-                    _context.SVM.Add(svm);
-                    await _context.SaveChangesAsync();
-                    Simulation simulation = new Simulation
-                    {
-                        UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
-                        idS = svm.id,
-                        time = DateTime.Now,
-                    };
-
-                    _context.Simulation.Add(simulation);
-                    await _context.SaveChangesAsync();
-
-                };
+                     _context.Add(simulation);
+                     await _context.SaveChangesAsync();
+               
 
 
-            }
+             }
+              if (checkbox_SVM == "SVM")
+             {
+                 SVM svm = new SVM();
+                 {
 
-            return default;
-        }
+                     svm.C = C;
+                     svm.kernel = kernel;
+                     svm.gamma = gamma;
+                     svm.coef0 = coef0;
+                     svm.tol = tol;
+                     svm.cache_size = cache_size;
+                     svm.max_iter = max_iter;
+                     svm.decision_function_shape = decision_function_shape;
+                 };
+                 _context.SVM.Add(svm);
+                     await _context.SaveChangesAsync();
+                     Simulation simulation = new Simulation
+                     {
+                         UserId = User.FindFirstValue(ClaimTypes.NameIdentifier),//Pour récupérer l'Id de la personne qui est connectée
+                         idS = svm.id,
+                         time = DateTime.Now,
+                     };
 
+                     _context.Add(simulation);
+                     await _context.SaveChangesAsync();
+
+                 
+
+
+             }
+
+             return View("Index");
+         }
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
